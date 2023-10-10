6b982fafcb25de07d3f765b544049425f4c07731
Ghaly Nicolas Jules <mghalynho@gmail.com>
2023-10-10 18:13:35 -0400

    Homepage structure done

--- lib/actions/thread.actions.ts
+++ lib/actions/thread.actions.ts
@@ -1,3 +1,4 @@
+"use server";
 import { revalidatePath } from "next/cache";
 import Thread from "../models/thread.model";
 import User from "../models/user.model";
@@ -10,20 +11,57 @@ interface Params {
   path: string;
 }
 
-const createThread = async ({ text, author, communityId, path }: Params) => {
+export const createThread = async ({
+  text,
+  author,
+  communityId,
+  path,
+}: Params) => {
   connectToDB();
 
-  const createThread = await Thread.create({
-    text,
-    author,
-    community: null,
-  });
+  try {
+    const createThread = await Thread.create({
+      text,
+      author,
+      community: null,
+    });
 
-  await User.findByIdAndUpdate(author, {
-    $push: { threads: createThread._id },
-  });
+    await User.findByIdAndUpdate(author, {
+      $push: { threads: createThread._id },
+    });
 
-  revalidatePath(path);
+    revalidatePath(path);
+  } catch (error: any) {
+    throw new Error(`Error creating Thread: ${error.message}`);
+  }
 };
 
-export default createThread;
+export const fetchThreads = async (pageNumber = 1, pageSize = 20) => {
+  connectToDB();
+
+  //calculate the numbe of posts to skip
+  const skipAmount = (pageNumber - 1) * pageSize;
+
+  //Fetch the posts that have no comments (top-level threads...)
+  const postsQuery = Thread.find({ parentId: { $in: [null, undefined] } })
+    .sort({ createdAt: "desc" })
+    .skip(skipAmount)
+    .limit(pageSize)
+    .populate({ path: "author", model: User })
+    .populate({
+      path: "children",
+      populate: {
+        path: "author",
+        model: User,
+        select: "_id name parentId image",
+      },
+    });
+
+  const totalPostsCount = await Thread.countDocuments({
+    parentId: { $in: [null, undefined] },
+  });
+
+  const posts = await postsQuery.exec();
+  const isNext = totalPostsCount > skipAmount + posts.length;
+  return { posts, isNext };
+};
