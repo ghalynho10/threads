2f77d1b0820e15637ba9c8eb77ae6329881bee4d
Ghaly Nicolas Jules <mghalynho@gmail.com>
2023-10-12 23:57:27 -0400

    created threadcard

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,4 +1,5 @@
 // 'use client'
+import ThreadCard from "@/components/cards/ThreadCard";
 import { fetchThreads } from "@/lib/actions/thread.actions";
 import { currentUser } from "@clerk/nextjs";
 
